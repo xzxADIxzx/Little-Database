0bdb76adf50fb5472d0a10ba1845d636ffa0b2a0
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2022-08-19 21:30:59 +0300

    add control class

---
+++ src/ldoa/Control.java
@@ -0,0 +1,12 @@
+package ldoa;
+
+import arc.ApplicationListener;
+import arc.util.Log;
+
+public class ServerControl implements ApplicationListener {
+
+    @Override
+    public void init() {
+        // TODO: handle user input
+    }
+}

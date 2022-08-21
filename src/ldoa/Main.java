45aead0dc2575ef70562e2a21c6a22541f9a3293
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-08-21 22:14:37 +0300

    prettier logging output

--- src/ldoa/Main.java
+++ src/ldoa/Main.java
@@ -4,17 +4,27 @@ import arc.backend.headless.HeadlessApplication;
 import arc.util.Log;
 import arc.util.Threads;
 
-import java.io.IOException; 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static final Server server = new Server();
     public static final Client client = new Client();
 
+    public static final String[] tags = { "&lc&fb[D]&fr", "&lb&fb[I]&fr", "&ly&fb[W]&fr", "&lr&fb[E]", "" };
+    public static final DateTimeFormatter dateTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
     public static Thread serverThread;
     public static Thread clientThread;
 
     public static void main(String[] args) {
+        Log.logger = (level, text) -> {
+            String result = Log.format("&lk&fb[" + dateTime.format(LocalDateTime.now()) + "]&fr " + tags[level.ordinal()] + " " + text + "&fr");
+            System.out.println(result);
+        };
+
         try {
             server.bind(6567, 6567);
 
@@ -23,7 +33,10 @@ public class Main {
 
             client.connect(5000, "127.0.0.1", 6567, 6567);
 
-            new HeadlessApplication(new Control, Log::err);
-        } catch (IOException error) { Log.err("Could not to initialize Little Database application", error); }
+            new HeadlessApplication(new Control(), Log::err);
+            Log.info("Little Database application loaded. Type @ for help.", "help");
+        } catch (IOException error) {
+            Log.err("Could not to load Little Database application", error);
+        }
     }
 }

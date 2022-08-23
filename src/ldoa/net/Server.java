5802883d1b115bb88aaf9d49a7a10a26acb08708
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-08-23 17:01:07 +0300

    add NetListener interface to Server

--- src/ldoa/net/Server.java
+++ src/ldoa/net/Server.java
@@ -1,8 +1,21 @@
 package ldoa.net;
 
-public class Server extends arc.net.Server {
+import arc.net.Connection;
+import arc.net.DcReason;
+import arc.net.NetListener;
+
+public class Server extends arc.net.Server implements NetListener {
 
     public Server() {
         super(32768, 8192, new PacketSerializer());
+        addListener(this);
     }
+
+    public void connected(Connection connection) {} // TODO check login and password or is it a local connection
+
+    public void disconnected(Connection connection, DcReason reason) {} // TODO stop all tasks created by this connection
+
+    public void received(Connection connection, Object object) {} // TODO handle ldrs
+
+    public void idle(Connection connection) {} // who needs this?
 }

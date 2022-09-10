877addfe07d9b68dade634c6a573d2ecfeab787b
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-09-10 22:15:05 +0300

    add simple ldr runner

--- src/ldoa/net/Database.java
+++ src/ldoa/net/Database.java
@@ -4,6 +4,7 @@ import arc.files.Fi;
 import arc.net.Connection;
 import arc.struct.ObjectMap;
 import arc.util.Log;
+import ldoa.net.ResponseMessage.*;
 import useful.Json;
 import useful.Json.JsonStyle;
 
@@ -11,7 +12,10 @@ import static arc.Core.*;
 
 public class Database {
 
-    public ObjectMap<String, Json> jsons = new ObjectMap<>();
+    public static final Object requestComplete = new Object();
+
+    public final ObjectMap<String, Json> jsons = new ObjectMap<>();
+    public Object context = requestComplete;
 
     public void load(String from) {
         jsons.clear();
@@ -37,7 +41,46 @@ public class Database {
     }
 
     public Object execute(Connection connection, String request) {
-        return null;
+        Log.info(request);
+        if (context == requestComplete) { // method called from outside
+            String[] file = request.split(" ", 2);
+            context = jsons.get(file[0]); // ldr start with filename
+
+            try {
+                if (context == null) return new ResponseMessage.RequestException() {{
+                    response = "File not found!"; // TODO add constuctor method
+                }};
+                else if (file.length == 1) return new ResponseMessage.RequestException() {{
+                    response = "Invalid LDR!";
+                }};
+                else return execute(connection, file[1]);
+            } finally { context = requestComplete; }
+        } else {
+            String[] command = request.split(" ", 2);
+            switch (command[0]) {
+                case "get" -> {
+                    if (command.length == 1) return new RequestException() {{
+                        response = "Too few arguments!";
+                    }};
+                    else if (context instanceof Json json) {
+                        String[] args = command[1].split(" ", 2);
+                        context = json.get(args[0]);
+
+                        if (args.length == 1) return new RequestSuccess() {{
+                            response = context == null ? null : context.toString();
+                        }};
+                        else return execute(connection, args[1]);
+                    } else return new RequestException() {{
+                        response = "Can not get value from non-json object!";
+                    }};
+                }
+                default -> {
+                    return new RequestException() {{
+                        response = "Invalid LDR!";
+                    }};
+                }
+            }
+        }
     }
 
     private void read(Fi file) {
